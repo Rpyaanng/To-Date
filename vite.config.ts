82ddd569954243a9b75ebd5999393cf41172d97d
Rpyaanng <RyanPang12@gmail.com>
2024-08-06 09:56:38 -0600

    Added ngrok support for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
-import { defineConfig } from "vite"
+import { ngrok } from 'vite-plugin-ngrok'
+import { defineConfig, loadEnv } from "vite"
 
-export default defineConfig({
-  plugins: [react(), TanStackRouterVite(),],
-  server: {
-    host: true,
-    watch: {
-    }
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react(), TanStackRouterVite(), ngrok({ authtoken: env.VITE_NGROK_AUTHTOKEN })],
+    server: {
+      host: true,
+      watch: {
+      }
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }
 })
